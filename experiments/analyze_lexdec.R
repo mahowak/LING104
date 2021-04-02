1b6f084630c9c16e27ff6ed3d53025ba9f393264
mahowak <kmahowald@gmail.com>
2021-04-01 17:03:19 -0700

    update for class2

--- experiments/analyze_lexdec.R
+++ experiments/analyze_lexdec.R
@@ -9,11 +9,11 @@ library(tidyverse)
 # 7. Group.
 # 8. Word number.
 # 9. Word.
-# 10. Reading time.
+# 10. Reading time (Reaction Time in milliseconds)
 # 11. KeyCode.
 # 12. Newline?
 # 13. Sentence (or sentence MD5).
-d = read_csv("experiments/lexdec.csv", comment = "#",
+d = read_csv("LING104/experiments/lexdec.csv", comment = "#",
              col_names = c("time", "subj", "exp",
                            "item", "element", "type",
                            "garbage", "wordnum", 
@@ -21,12 +21,30 @@ d = read_csv("experiments/lexdec.csv", comment = "#",
                            "Sent")) %>%
   filter(type != "words")
 
-d$correct = (d$guess == "S" & d$type == "wordsYES") | 
-            (d$guess == "K" & d$type == "wordsNO")
 
+# says if it's correct
+# it's correct if: person pressed S and it YES is a word
+# it's correct if: person pressed K and it NO is not a word
+# it's incorrect: all other times
+d$correct = (d$guess == "S" & d$type == "wordsYES") | 
+  (d$guess == "K" & d$type == "wordsNO")
+mean(d$correct)
 
 unique(d$word)
 
 select(d, word, type)
 
+head(d)
+summary(d)
 
+####
+hist(d$RT, breaks=50)
+max(d$RT)
+head(d)
+nrow(d)
+
+summary(d$correct)
+
+hist(filter(d)$RT, breaks=50)
+hist(filter(d, correct == TRUE)$RT, breaks=50)
+hist(filter(d, correct == FALSE)$RT, breaks=50)
